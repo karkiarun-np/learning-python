e46f57eed5e0fe2f56a982e2e29f34d9e78cdfec
kshatreearun <kshatree.arun@gmail.com>
2026-03-21 19:06:52 +0545

    Initial push to main branch

---
+++ FirstProgram.py
@@ -0,0 +1,12 @@
+# Getting student details and print
+name=input("what is your name?")
+grade=input("what is your grade?")
+email=input("what is your email?")
+age=input("what is your age?")
+
+print("Student Details")
+print("--------------")
+print("Name: ",name)
+print("Grade: ",grade)
+print("Email: ",email)
+print("Age: ",age)
---
+++ Test.py
@@ -0,0 +1,26 @@
+# Swapping two variable
+#=======================
+"""
+a= input("Enter First number: ")
+b= input("Enter Second number: ")
+
+#temp=a
+#a=b
+#b=temp
+a,b=b,a
+print("First Number:",a)
+print("Second Number:",b)
+"""
+
+# Variable type checking and conversion
+#======================================
+"""
+age=float(input("Enter Age: "))
+print(type(age))
+"""
+
+# Testing String Function
+sentence=input("Enter Sentence: ")
+print(type(sentence))
+b=("Your Sentence is {}")
+print (b.format(sentence))
